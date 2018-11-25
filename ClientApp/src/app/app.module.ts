ec8266f83b733e1f9fd5033e03581b898636e7ec
Marc Vaccaro <marcgvaccaro@gmail.com>
2018-11-25 08:36:38 -0800

    Partially implement add vehicle form

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -9,6 +9,9 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { VehicleFormComponent } from './vehicle-form/vehicle-form.component';
+import { MakeService } from './services/make.service';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
@@ -16,19 +19,24 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    VehicleFormComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
+    HttpModule,
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'vehicles/new', component: VehicleFormComponent },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
     ])
   ],
-  providers: [],
+  providers: [
+    MakeService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ ClientApp/src/app/vehicle-form/vehicle-form.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from "@angular/core";
+import { MakeService } from "../services/make.service";
+
+@Component({
+  selector: 'vehicle-form',
+  templateUrl: './vehicle-form.component.html',
+  styleUrls: ['./vehicle-form.component.css']
+
+
+})
+export class VehicleFormComponent implements OnInit {
+
+  makes;
+
+  constructor(private makeService: MakeService) {}
+
+  ngOnInit() {
+    this.makeService.getMakes().subscribe(makes => {
+      this.makes = makes;
+      console.log("Makes", this.makes)
+    })
+  }
+
+}
