fd43fadadcd059ba70915a75a1852c636d55c702
Marc Vaccaro <marcgvaccaro@gmail.com>
2018-11-24 21:25:02 -0800

    Add/Implement EntityFramework/DbContext & seed data

---
+++ Persistence/VegaDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vega.Models;
+
+namespace Vega.Persistence
+{
+    public class VegaDbContext : DbContext
+    {
+        public DbSet<Make> Makes { get; set; }
+
+        //don't need to explicitly declare this table since we won't directly query it
+        //public DbSet<Model> Models { get; set; }
+
+        public VegaDbContext(DbContextOptions<VegaDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
