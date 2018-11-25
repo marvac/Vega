871e654ecd9bfbe147bf702066317041e324ff2d
Marc Vaccaro <marcgvaccaro@gmail.com>
2018-11-24 20:20:58 -0800

    Initial files & cleanup

--- Controllers/SampleDataController.cs
+++ Controllers/SampleDataController.cs
@@ -11,7 +11,7 @@ namespace Vega.Controllers
     {
         private static string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Cold", "Mild", "Hot"
         };
 
         [HttpGet("[action]")]
---
+++ README.md
@@ -0,0 +1,15 @@
+# Vega
+
+Sample web application for adding Angular on top of an ASP.NET core backend
+
+## Quick Start
+
+In the terminal at the root of this project: 
+
+`npm install`
+
+Then run the project from Visual Studio (ctrl + F5)
+
+## Credit
+
+https://www.udemy.com/aspnet-core-angular
