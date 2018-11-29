c7cf65925db0f1aa631681bf807d0d3aaa7fef9f
Marc Vaccaro <marcgvaccaro@gmail.com>
2018-11-28 22:54:24 -0800

    Implement vehicle API resource mapping

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -1,30 +1,49 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Vega.Controllers.Resources;
 using Vega.Models;
+using Vega.Persistence;
 
 namespace Vega.Controllers
 {
     [Route("/api/vehicles")]
     public class VehiclesController : Controller
     {
-        public VehiclesController()
-        {
+        private readonly IMapper _mapper;
+        private readonly VegaDbContext _context;
 
+        public VehiclesController(IMapper mapper, VegaDbContext context)
+        {
+            _mapper = mapper;
+            _context = context;
         }
 
         [HttpGet()]
         public IActionResult GetVehicles()
         {
-            return new NotFoundResult();
+            return NotFound("GET not implemented");
         }
 
         [HttpPost]
-        public IActionResult CreateVehicle(Vehicle vehicle)
+        public async Task<IActionResult> CreateVehicle([FromBody]VehicleResource resource)
         {
-            return Ok(vehicle);
+            var vehicle = _mapper.Map<VehicleResource, Vehicle>(resource);
+            vehicle.LastUpdated = DateTime.Now;
+
+            _context.Add(vehicle);
+
+            int results = await _context.SaveChangesAsync();
+            if (results > 0)
+            {
+                var result = _mapper.Map<Vehicle, VehicleResource>(vehicle);
+                return Ok(result);
+            }
+
+            return NotFound("Could not save object");
         }
     }
 }
--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -13,9 +13,24 @@ namespace Vega.Mapping
     {
         public MappingProfile()
         {
-            //append .ReverseMap() to allow 2-way mapping
+            /* Domain to API Resource */
+
+            //append .ReverseMap() to allow 2-way mapping (if no extra params)
             CreateMap<Make, MakeResource>();
             CreateMap<Model, ModelResource>();
+            CreateMap<Vehicle, VehicleResource>()
+                .ForMember(r => r.Contact, options => options.MapFrom(v => new ContactResource { Name = v.ContactName, Phone = v.ContactPhone, Email = v.ContactEmail }))
+                .ForMember(r => r.Features, options => options.MapFrom(v => v.Features.Select(f => f.FeatureId)));
+
+            //CreateMap<Feature, FeatureResource>();
+
+
+            /* API resource to Domain */
+            CreateMap<VehicleResource, Vehicle>()
+                .ForMember(v => v.ContactName, options => options.MapFrom(r => r.Contact.Name))
+                .ForMember(v => v.ContactPhone, options => options.MapFrom(r => r.Contact.Phone))
+                .ForMember(v => v.ContactEmail, options => options.MapFrom(r => r.Contact.Email))
+                .ForMember(v => v.Features, options => options.MapFrom(r => r.Features.Select(id => new VehicleFeature { FeatureId = id })));
         }
     }
 }
