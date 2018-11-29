f4f4380a46c86de4d90d788dd15067f0c6d659ed
Marc Vaccaro <marcgvaccaro@gmail.com>
2018-11-28 22:18:19 -0800

    Add VehicleFeature implementation

---
+++ Controllers/VehiclesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vega.Models;
+
+namespace Vega.Controllers
+{
+    [Route("/api/vehicles")]
+    public class VehiclesController : Controller
+    {
+        public VehiclesController()
+        {
+
+        }
+
+        [HttpGet()]
+        public IActionResult GetVehicles()
+        {
+            return new NotFoundResult();
+        }
+
+        [HttpPost]
+        public IActionResult CreateVehicle(Vehicle vehicle)
+        {
+            return Ok(vehicle);
+        }
+    }
+}
--- Persistence/VegaDbContext.cs
+++ Persistence/VegaDbContext.cs
@@ -10,6 +10,7 @@ namespace Vega.Persistence
     public class VegaDbContext : DbContext
     {
         public DbSet<Make> Makes { get; set; }
+        public DbSet<Vehicle> Vehicles { get; set; }
         public DbSet<Feature> Features { get; set; }
 
         //don't need to explicitly declare this table since we won't directly query it
@@ -18,6 +19,11 @@ namespace Vega.Persistence
         public VegaDbContext(DbContextOptions<VegaDbContext> options) : base(options)
         {
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<VehicleFeature>().HasKey(v => new { v.VehicleId, v.FeatureId });
         }
     }
 }
